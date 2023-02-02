c667538126db0b143ee390f1a5b5f49ad62cb735
Adrian Goding <godingadrian@gmail.com>
2023-02-01 19:02:28 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(){
+// function distanceFromHqInBlocks(someValue){
+//     distanceFromHqInBlocks(5)
+//     return distanceFromHqInBlocks
+// }
+function distanceFromHqInBlocks(blockNumber){
+    if (blockNumber > 42) {
+        return blockNumber - 42; 
+    } else if (blockNumber < 42){
+        return 42 - blockNumber;
+    }
 }
-    return distanceFromHqInBlocks
+
+function distanceFromHqInFeet(feetNumber){
+    return distanceFromHqInBlocks(feetNumber) * 264
+}
+
+function distanceTravelledInFeet(start, destintation){
+    if (start > destintation){
+        return (start - destintation) * 264; 
+    } else if (start < destintation) {
+        return (destintation - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destintation){
+    const distance = distanceTravelledInFeet(start, destintation)
+    
+    if (distance <= 400){
+        return 0; 
+    } else if (distance >= 400 && distance < 2000){
+        return .02 * (distance - 400);
+    }else if (distance >= 2000 && distance < 2500){
+        return 25; 
+    }else {
+        return 'cannot travel that far'
+    }
+
+}
